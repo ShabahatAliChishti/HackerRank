454f835febcd4bf7e5098f4278e6a1c2ecbde916
Shabahat Ali <shabahatalichishti@outlook.com>
2021-10-12 00:10:02 +0500

    Q4

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,30 +10,42 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            //Calculate and print the sum of the elements in an array.keeping in mind that some of those integeres may a quite large.
-            //input format :the first line of the input consists of an integer n.
-            //the next line contains n space-spearated integers contained in the array
-            //output:print the sum of the elements in the array
-            //Sample input:
-            //5 
-            //10000001 10000002 1000000003 1000000004 100000005
-
-            //output:500000015
-            long []ar;
-            ar = new long[] { 1000000001, 1000000002, 1000000003 };
-
-            Console.WriteLine(averyBigSum(ar));
+           //insert a node at a specific position in a linked list
+           //Q:given some data  and will be given position and so they expect us to make a node with the data and then insert into our linkedlist at the given desire position
 
 
         }
-        static long  averyBigSum(long[] ar)
+        static SinglyLinkedListNode  insertNodeAtPosition(SinglyLinkedListNode head,int data,int position)
         {
-            long sum = 0;
-          for(int i=0;i<ar.Length;i++)
+            //initialize node on singly linked list with data 
+            SinglyLinkedListNode node = new SinglyLinkedListNode(data);
+            //to look at every single node that's called current node and it starting with head
+            SinglyLinkedListNode currentNode = head;
+            //advance the currentnode until we get to the node that is right before the position what we want to insert node at
+            if(head==null)
             {
-                sum += ar[i];
+                return node;
             }
-            return sum;
+            for(int i=0;i<position-1;i++)
+            {
+                //iterating towards next node and set that node into currentnode
+                currentNode = currentNode.next;
+            }
+            //for change references we have to remember we have to do that at specific order so before we can remove the reference from current node we have to say node next is equal to current node and therefore we hold on that reference and we make sure it doesn't get chopped when we do the next line which is saying that the current node next is equal to node
+            node.next = currentNode.next;
+            currentNode.next = head;
+            return head;
+        }
+        public class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+           public SinglyLinkedListNode(int data)
+            {
+                this.data = data;
+            }
+
         }
        
     }
