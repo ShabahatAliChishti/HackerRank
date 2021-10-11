6dd9e62b46c458cd02d2cf99a8893ed1c2a17eab
Shabahat Ali <shabahatalichishti@outlook.com>
2021-10-12 03:12:34 +0500

    Q10

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -10,36 +10,21 @@ namespace HackerRank
     {
         static void Main(string[] args)
         {
-            //Insert Node at the Tail of a Linked List
+            //print each element in linked list
 
 
         }
-        static SinglyLinkedListNode  insertNodeAtTail(SinglyLinkedListNode head,int data,int position)
+        static void  printLinkedList(SinglyLinkedListNode head)
         {
-            //initialize node on singly linked list with data 
-            SinglyLinkedListNode node = new SinglyLinkedListNode(data);
-          
-            //advance the currentnode until we get to the node that is right before the position what we want to insert node at
-            if(head==null)
+            SinglyLinkedListNode node = head;
+             while(node!=null)
             {
-                return node;
+                Console.WriteLine(node.data);
+                node = node.next;
             }
-            else
-            {
-                //currentnode ki inialization head se hogi na
-                var currentNode = head;
-                //humaain next jb tk chalana hai jb tk akhiri node null ni miljati
-                while(currentNode.next!=null)
-                {
-                    currentNode = currentNode.next;
-                }
-                //jb mil gayi node null tou us null ki jga humain apna data dalna hai means apni node
-                currentNode.next = node;
+           
 
 
-            }
-            //because head modified hojayega jb hum null ki jga value insert krdengy node ki apny
-            return head;
         }
         public class SinglyLinkedListNode
         {
