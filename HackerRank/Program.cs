2d1968ca2f455ec077cf8a2991a09edd18290eed
Shabahat Ali <shabahatalichishti@outlook.com>
2021-10-12 08:14:03 +0500

    Q4 modified

--- HackerRank/Program.cs
+++ HackerRank/Program.cs
@@ -17,24 +17,20 @@ namespace HackerRank
         }
         static SinglyLinkedListNode  insertNodeAtPosition(SinglyLinkedListNode head,int data,int position)
         {
-            //initialize node on singly linked list with data 
             SinglyLinkedListNode node = new SinglyLinkedListNode(data);
-            //to look at every single node that's called current node and it starting with head
             SinglyLinkedListNode currentNode = head;
-            //advance the currentnode until we get to the node that is right before the position what we want to insert node at
             if(head==null)
             {
                 return node;
             }
-            for(int i=0;i<position-1;i++)
+            for (int i = 0; i < position-1; i++)
             {
-                //iterating towards next node and set that node into currentnode
                 currentNode = currentNode.next;
             }
-            //for change references we have to remember we have to do that at specific order so before we can remove the reference from current node we have to say node next is equal to current node and therefore we hold on that reference and we make sure it doesn't get chopped when we do the next line which is saying that the current node next is equal to node
             node.next = currentNode.next;
-            currentNode.next = head;
+            currentNode.next = node;
             return head;
+
         }
         public class SinglyLinkedListNode
         {
